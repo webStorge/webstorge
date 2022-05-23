f3840e9051f7b96ebcb7a32d70b91c040c7f0c4f
kungchi <a01027756032@gmail.com>
2022-05-24 03:38:53 +0900

    용량 단위 계산 추가

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php include  $_SERVER['DOCUMENT_ROOT']."/front-back(상훈)/php/db.php"; ?>
+<?php include  $_SERVER['DOCUMENT_ROOT']."/front-back(sh)/php/db.php"; ?>
 <!doctype html>
 <html lang="en">
   <head>
@@ -78,18 +78,18 @@
   </head>
   <body>
     
-<header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
-  <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6" href="./index.php">Cloud Box</a>
-  <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <!-- <input class="form-control form-control-dark w-100 rounded-0 border-0" type="text" placeholder="Search" aria-label="Search"> -->
-  <div class="navbar-nav">
-    <div class="nav-item text-nowrap">
-      <a class="nav-link px-3" href="login/Logout.php">Log out</a>
-    </div>
-  </div>
-</header>
+	<header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
+	  <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6" href="./index.php">Cloud Box</a>
+	  <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+	    <span class="navbar-toggler-icon"></span>
+	  </button>
+	  <!-- <input class="form-control form-control-dark w-100 rounded-0 border-0" type="text" placeholder="Search" aria-label="Search"> -->
+	  <div class="navbar-nav">
+	    <div class="nav-item text-nowrap">
+	      <a class="nav-link px-3" href="login/Logout.php">Log out</a>
+	    </div>
+	  </div>
+	</header>
 
 <div class="container-fluid">
   <div class="row">
@@ -103,8 +103,6 @@
                 </svg>
               모든 파일
             </a>
-          </li>
-          <li class="nav-item">
           </li>
           <li class="nav-item">
             <a class="nav-link" href="./favorites-page.html">
@@ -130,15 +128,13 @@
     <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
       <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
         <h1 class="h2">모든 파일</h1>
-        	<form action="./php/upload.php" method="post" enctype="multipart/form-data">
+        	<form action="php/upload.php" method="post" enctype="multipart/form-data">
 	        	<div class="btn-group me-2">    
-	            	<input id="my-input" type="file" name='my-input' onchange="form.submit()"/>
+	            	<input id="my-input" type="file" name='foo' onchange="form.submit()"/>
 	            	<button type="button" id="uploadBtn" class="btn btn-sm btn-outline-secondary" onclick='onClickUpload()'>&nbsp업로드&nbsp</button>
 	        	</div>
 	        </form>
       </div>
-
-      <!-- <canvas class="my-4 w-100" id="myChart" width="900" height="380"></canvas> -->
       <div class="table-responsive">
         <table class="table table-striped table-sm">
           <thead>
@@ -158,9 +154,26 @@
 		            <tr>
 		              <td><font size="5"><?php echo $i; $i = $i +1;?></font></td>
 		              <td onClick = "location.href='php/download.php?file=<?php echo $board['uname'] ?>'"><font size="5"><?php echo $board['name']; ?></font></td>
-		              <td><font size="5"><?php echo $board['size']; ?></font></td>
+		              <td>
+			              <font size="5">
+				              <?php 
+								    $value = $board['size'];
+								    if($value < 1024) {
+								        $value= round($value,2)  . "&nbsp Bytes";
+								    }
+								    else if($value < 1024000) {
+								       $value = round(($value / 1024 ),2) . "&nbsp KB";
+								    }
+								    else {
+								        $value = round(($value / 1024000),2) . "&nbsp MB";
+								    }
+								    echo $value;
+									
+				              ?>
+			            	</font>
+		              </td>
 		              <td><font size="5"><?php echo $board['date']; ?></font></td>
-		              <td><button type="button" id="btn" class="btn btn-warning"><font size="4">즐겨찾기</font></button></td>
+		              <td style='width:120px;'><button type="button" id="btn" class="btn btn-warning"><font size="4">즐겨찾기</font></button></td>
 		              <td><button type="button" id="btn" class="btn btn-secondary btn-default" onClick = "location.href='php/delete.php?uname=<?php echo $board['uname'] ?>'"><font size="4">삭제</font></button></td>
 		            </tr>
 		        </tbody>
