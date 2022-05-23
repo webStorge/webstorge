79f07a1e241973aab40ad3d0ba6323de091ea345
kungchi <a01027756032@gmail.com>
2022-05-24 01:08:52 +0900

    상단바 메뉴 위치 조정

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
     <title>CloudBox</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="./css/style.css?after">
     <link rel="shortcut icon" type="image/x-icon" href="./icon_images/favicon.ico" />
     
 
